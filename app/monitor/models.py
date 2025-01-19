204f065104a75dc438b38eb9cf3851dcf3f977fc
Amal Babu <amalbabu1200@gmail.com>
2025-01-19 11:29:47 +0530

    format

--- app/monitor/models.py
+++ app/monitor/models.py
@@ -8,6 +8,9 @@ class Site(models.Model):
     expected_status_code = models.IntegerField(default=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name or self.url
+
 class SiteStatusHistory(models.Model):
     site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='history')
     status = models.CharField(max_length=10, choices=[('up', 'Up'), ('down', 'Down')])
@@ -15,7 +18,13 @@ class SiteStatusHistory(models.Model):
     checked_at = models.DateTimeField(auto_now_add=True)
     status_change = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.site} - {self.status} - {self.checked_at}"
+
 class Webhook(models.Model):
     url = models.URLField(unique=True)
     description = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.url
